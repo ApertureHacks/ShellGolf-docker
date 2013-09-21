efdf364e11eaf10b90569b1ff5c5715fc495dbdf
katiepru <katiepru@nbcs.rutgers.edu>
2013-09-21 15:40:46 -0400

    moving app.route

--- app.js
+++ app.js
@@ -74,12 +74,12 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.cookieParser());
 app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(app.router);
 
 // development only
 if ('development' === app.get('env')) {
