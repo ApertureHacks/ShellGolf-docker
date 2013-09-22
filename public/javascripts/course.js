1f69ce0a3c4930c744db4357afa70f7b0f2c806f
katiepru <katiepru@nbcs.rutgers.edu>
2013-09-22 08:31:42 -0400

    adding stats

--- public/javascripts/course.js
+++ public/javascripts/course.js
@@ -1,6 +1,8 @@
 $("#submit_code").click(function(btn){
   $btn = $(this);
   $commands_field = $("#commands");
+  $stats_sidebar = $('#stats_sidebar');
+  $sidebar_info = $('#sidebar_info');
 
   $btn.removeClass("btn-default");
   $btn.addClass("btn-info");
@@ -14,8 +16,13 @@ $("#submit_code").click(function(btn){
       console.log(data);
       data = JSON.parse(data);
       if (data.success) {
+        var strlen = commands.length;
+        var cmds = (commands.split(";")).length + (commands.split("|")).length;
         $btn.removeClass("btn-info");
         $btn.addClass("btn-success");
+        $stats_sidebar.text("Success");
+        $sidebar_info.text("Number of chars: " + strlen +
+                           "\nNumber of commands: " + cmds + "\nResult: Par");
       } else {
         $btn.removeClass("btn-info");
         $btn.addClass("btn-warning");
