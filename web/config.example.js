1a57b7bb4fc255463551e863bc7ae4fd7027a60a
Josh Matthews <josh@jmatthews.us>
2014-01-01 16:03:37 -0500

    Splitting docker and web parts into separate repos. This is the docker repo.
    Moving everything from docker into the root.

--- web/config.example.js
+++
@@ -1,9 +0,0 @@
-exports.twitter_auth = { consumerKey: '<key>'
-                       , consumerSecret: '<secret>'
-                       , callbackURL: '<callback url>' };
-
-exports.db = { uri: 'mongodb://<mongo host>:<mongo port>/<db name>'
-             , user: '<mongo user>'
-             , pass: '<user password>' };
-
-exports.session_secret = '<session secret>';
